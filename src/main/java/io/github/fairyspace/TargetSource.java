c646c115b5f28450726df63ca2b2db0cc2ce9a6d
quanruxu <krhudsong@gmail.com>
2023-07-06 18:22:39 +0800

    实现注解@Value，@autowire

---
+++ src/main/java/io/github/fairyspace/TargetSource.java
@@ -0,0 +1,25 @@
+package io.github.fairyspace;
+
+public class TargetSource {
+    private final Object target;
+
+    public TargetSource(Object target) {
+        this.target = target;
+    }
+
+    /**
+     * Return the type of targets returned by this {@link TargetSource}.
+     * <p>Can return <code>null</code>, although certain usages of a
+     * <code>TargetSource</code> might just work with a predetermined
+     * target class.
+     * @return the type of targets returned by this {@link TargetSource}
+     */
+    public Class<?>[] getTargetClass(){
+        return this.target.getClass().getInterfaces();
+    }
+
+    public Object getTarget(){
+        return this.target;
+    }
+
+}
---
+++ src/main/java/io/github/fairyspace/aop/framework/autoproxy/DefaultAdvisorAutoProxyCreator.java
@@ -0,0 +1,75 @@
+package io.github.fairyspace.aop.framework.autoproxy;
+
+import io.github.fairyspace.TargetSource;
+import io.github.fairyspace.aop.AdvisedSupport;
+import io.github.fairyspace.aop.Advisor;
+import io.github.fairyspace.aop.ClassFilter;
+import io.github.fairyspace.aop.Pointcut;
+import io.github.fairyspace.aop.aspectj.AspectJExpressionPointcutAdvisor;
+import io.github.fairyspace.aop.framework.ProxyFactory;
+import io.github.fairyspace.beans.BeanFactory;
+import io.github.fairyspace.beans.PropertyValues;
+import io.github.fairyspace.beans.factory.BeanFactoryAware;
+import io.github.fairyspace.beans.factory.config.InstantiationAwareBeanPostProcessor;
+import io.github.fairyspace.beans.factory.support.DefaultListableBeanFactory;
+import org.aopalliance.aop.Advice;
+import org.aopalliance.intercept.MethodInterceptor;
+
+import java.util.Collection;
+
+public class DefaultAdvisorAutoProxyCreator implements InstantiationAwareBeanPostProcessor, BeanFactoryAware {
+    private DefaultListableBeanFactory beanFactory;
+
+
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) {
+        this.beanFactory = (DefaultListableBeanFactory) beanFactory;
+    }
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) {
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) {
+        return bean;
+    }
+
+    @Override
+    public Object postProcessBeforeInstantiation(Class<?> beanClass, String beanName) {
+        //避免死循环
+        if (isInfrastructureClass(beanClass)) return null;
+        Collection<AspectJExpressionPointcutAdvisor> advisors = beanFactory.getBeansOfType(AspectJExpressionPointcutAdvisor.class).values();
+        for (AspectJExpressionPointcutAdvisor advisor : advisors) {
+            ClassFilter classFilter = advisor.getPointcut().getClassFilter();
+            if (!classFilter.matches(beanClass)) continue;
+            AdvisedSupport advisedSupport = new AdvisedSupport();
+            TargetSource targetSource = null;
+            try {
+                targetSource = new TargetSource(beanClass.getDeclaredConstructor().newInstance());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            advisedSupport.setTargetSource(targetSource);
+            advisedSupport.setMethodInterceptor((MethodInterceptor) advisor.getAdvice());
+            advisedSupport.setMethodMatcher(advisor.getPointcut().getMethodMatcher());
+            advisedSupport.setProxyTargetClass(false);
+            return new ProxyFactory(advisedSupport).getProxy();
+
+        }
+        return null;
+    }
+
+    @Override
+    public PropertyValues postProcessPropertyValues(PropertyValues pvs, Object bean, String beanName) {
+        return pvs;
+    }
+
+    private boolean isInfrastructureClass(Class<?> beanClass) {
+        return Advice.class.isAssignableFrom(beanClass)
+                || Pointcut.class.isAssignableFrom(beanClass)
+                || Advisor.class.isAssignableFrom(beanClass);
+    }
+}
